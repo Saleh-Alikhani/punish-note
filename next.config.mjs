b038d46b045d6faba39c23de8feb857c3d4a728b
Saleh Alikhani <salehalikhani@gmail.com>
2025-01-08 09:25:55 +0330

    boro git

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages:[
+    'rc-util',
+    'rc-input',
+    '@ant-design',
+    'rc-pagination',
+    'rc-picker',
+    'antd'
+  ]
 };
 
 export default nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,35 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { Divider, Form, Input, Select } from "antd";
+import { useState } from "react";
+import { useForm } from "antd/es/form/Form";
+
+
 
 export default function Home() {
+  const [form] = useForm()
+  const [state,setState] = useState<{
+    hex:string | null;
+    jorm:string | null;
+  }>({
+    hex:null,jorm:null
+  })
+  const [preview,setPreview] = useState<string>('')
+  const createNote= ()=>{
+    const fields = form.getFieldsValue()
+    return `\`\`\`1 .  Time : ${fields.time}
+2 .  Steam Hex afard : ${state.hex}
+3 . Punishments : ${state.jorm}
+4 . Judge : ${fields.judge}
+5 . Steam hex shaki (id discordfard  ): ${fields.shaki}
+ Nahve daryaft shekayat ( report , ticket , ....  )  : ${fields.rah}\`\`\``
+  }
+   const handleFinish = ()=>{
+    navigator.clipboard.writeText(createNote())
+    form.resetFields()
+    setState({hex:null,jorm:null})
+   }
+
   return (
     <>
       <Head>
@@ -14,100 +38,35 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+      <main>
+          <button onClick={()=>navigator.clipboard.readText().then(r=> 
+      setState({hex:r.split(' ')[2] ,jorm:r.split(':')[2]})
+    )}>{state.hex?.includes('steam')?'Set shod':'Click me'}</button>
+        <Divider/>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
+          {state.hex}     -    {state.jorm}
+        <Divider/>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        <Form onFinish={()=>handleFinish()} layout="vertical" form={form}>
+          <Form.Item label='shaki' name='shaki'>
+            <Input />
+          </Form.Item>
+          <Form.Item label='time' name='time'>
+            <Input />
+          </Form.Item>
+          <Form.Item label='judge' name='judge'>
+            <Input />
+          </Form.Item>
+          <Form.Item initialValue='Temp' label='nahve ersal' name='rah'>
+            <Select defaultValue='Temp' options={[{title:'Temp',value:'Temp'},{title:'Report',value:'Report'},{title:'Ticket',value:'Ticket'}]} />
+          </Form.Item>
+          <button type="submit">get note</button>
+        </Form>
+        <Divider/>
+        <button onClick={()=>setPreview(createNote)}>preview</button>
+        <button onClick={()=>setPreview('')}>hide preview</button>
+        <Divider/>
+        {preview !== '' && preview}
       </main>
     </>
   );
